5adaece2670b4383eb70b06a42c73a29a2cd0457
ShingiraiBhengesa <72065045+ShingiraiBhengesa@users.noreply.github.com>
2022-05-12 19:02:04 +0300

    update functions

--- src/index.js
+++ src/index.js
@@ -1,44 +1,80 @@
 import './styles.css';
-const player=document.querySelector(".name")
-const score=document.querySelector(".score")
-const submitbtn=document.querySelector(".submit")
-let api = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/"
-let gameid
- const gamename= async (gameid)=>{
-    fetch(api,{
-    method: "POST",
-    body: JSON.stringify({ "name": "Shingirai's Game" })
-    ,
+
+const player = document.querySelector('.name');
+const score = document.querySelector('.score');
+const submitbtn = document.querySelector('.submit');
+const refreshbtn = document.querySelector('.refresh');
+const scoresdiv = document.querySelector('.scores-div');
+const msg = document.querySelector('#message');
+const gameid = localStorage.getItem('scores');
+const api = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+
+const hide = () => {
+  msg.classList.remove('active');
+  msg.classList.remove('empty');
+};
+
+const gamename = async (gameid) => {
+  let response = await fetch(api, {
+    method: 'POST',
+    body: JSON.stringify({ name: "Shingirais's Game" }),
     headers: {
-        "Content-type": "application/json; charset=UTF-8"
-    }
-    })
-    .then(response=>response.json())
-    .then(data=>{gameid=data.result.slice(14,34)
-    console.log(gameid)
-})
-}
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  response = await response.json();
+  gameid = await response.result.slice(14, 34);
+  localStorage.setItem('scores', gameid);
+  window.location.reload();
+};
 
+const submitfun = async (player, score, gameid) => {
+  if (player.value !== '' && score.value !== '') {
+    await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameid}/scores`, {
+      method: 'POST',
+      body: JSON.stringify({
+        user: `${player.value}`,
+        score: `${score.value}`,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    msg.textContent = 'Score Added';
+    msg.classList.add('active');
+    player.value = '';
+    score.value = '';
+    setTimeout(hide, 2000);
+  } else {
+    msg.textContent = "Inputs shouldn't be empty";
+    msg.classList.add('empty');
+    setTimeout(hide, 2000);
+  }
+};
 
-const submitfun= async(player,score,gameid)=>{
-    if(player.value!=="" && score.value!==""){
-    fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameid}/scores`,{
-    method: "POST",
-    body: JSON.stringify({
-        "user": `"${player.value}"`,
-        "score": `"${score.value}"`
-    })
-    ,
+const refreshfun = async (gameid, scoresdiv) => {
+  msg.textContent = 'Getting Data';
+  msg.classList.add('active');
+  setTimeout(hide, 2000);
+  scoresdiv.innerHTML = '';
+  let response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameid}/scores`, {
+    method: 'GET',
     headers: {
-        "Content-type": "application/json; charset=UTF-8"
-    }
-    })
-    .then(response=>response.json())
-    .then(data=>console.log(data))
-    }
-}
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  response = await response.json();
+  response.result.forEach((element) => {
+    scoresdiv.innerHTML += `<li class="scores-li">${element.user}  ${element.score}</li>`;
+  });
+};
 
+if (gameid === null) { gamename(); }
 
-submitbtn.addEventListener("click",()=>{
-    submitfun(player,score,gameid)
-})
+submitbtn.addEventListener('click', () => {
+  submitfun(player, score, gameid);
+});
+
+refreshbtn.addEventListener('click', () => {
+  refreshfun(gameid, scoresdiv);
+});
